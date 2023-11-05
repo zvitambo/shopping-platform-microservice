52de5d80709fd3fc466604c749c61a27fc1fd8b1
Thomas Jindu <thomas@spectrumits.co.zw>
2023-11-05 13:24:56 +0200

    initial commit

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,16 @@
+// which service it is
+describe("CustomerService", () => {
+  // This will explain which function we are testing!
+  describe("SignIn", () => {
+    // Which Scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("Validate response", async () => {});
+  });
+
+  describe('SignUp', () =>{
+    test("validate user inputs", () => {});
+
+    test("Validate response", async () => {});
+  })
+});
